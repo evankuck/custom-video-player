6c338843614b1b217ac74578aa76ca7a1efb30f6
evankuck <86078315+evankuck@users.noreply.github.com>
2023-07-15 16:43:35 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
 /* Get Our Elements */
 const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+/* Build our Functions */
+
+
+/* Hook up event listeners */
