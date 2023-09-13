dc70ac425f98108e64f165cdb44fcdb77c9ac06b
evankuck <86078315+evankuck@users.noreply.github.com>
2023-09-12 22:47:07 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,17 +25,26 @@ function skip() {
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
-  console.log(this.name);
-  console.log(this.value);
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.computedStyleMap.flexBasis = `${percent}%`;
+}
+
+function scrub() {
+  console.log(e);
+}
 
 /* Hook up event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', togglePlay);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
